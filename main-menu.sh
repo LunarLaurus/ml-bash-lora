857ec9c0bea9f3efe0a5d6fe2bef96f02b417067
lunarlaurus <draknyte1@gmail.com>
2025-10-07 20:20:24 +0100

    Menu fix.

--- main-menu.sh
+++ main-menu.sh
@@ -9,19 +9,17 @@ while true; do
     echo -e "1) NVIDIA Drivers"
     echo -e "2) CUDA Toolkit"
     echo -e "3) Conda / ML Environments"
-    echo -e "4) ===="
-    echo -e "5) Diagnostics"
-    echo -e "6) Utilities"
-    echo -e "0) Exit"
+    echo -e "4) Diagnostics"
+    echo -e "5) Utilities"
+    echo -e "${RED}0) Exit${NC}"
 
     read -rp "Choice: " choice
     case $choice in
         1) source ./drivers/drivers-menu.sh ;;
         2) source ./cuda/cuda-menu.sh ;;
         3) source ./conda/conda-menu.sh ;;
-        #4) source ./pytorch/pytorch-menu.sh ;;
-        5) source ./diagnostics/diagnostics-menu.sh ;;
-        6) source ./utils/utils-menu.sh ;;
+        4) source ./diagnostics/diagnostics-menu.sh ;;
+        5) source ./utils/utils-menu.sh ;;
         0) echo "Exiting."; exit 0 ;;
         *) echo -e "${RED}Invalid option${NC}" ;;
     esac
